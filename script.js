d3931fc8c1c3572a9d8aaee607856c4519039ccd
hartioTOK <hartioaja@gmail.com>
2025-06-26 11:15:27 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const noBtn = document.getElementById('noBtn');
-
-noBtn.addEventListener('mouseover', () => {
-  const randomX = Math.floor(Math.random() * (window.innerWidth - 100));
-  const randomY = Math.floor(Math.random() * (window.innerHeight - 50));
-  noBtn.style.left = `${randomX}px`;
-  noBtn.style.top = `${randomY}px`;
-});
